de67e6e3df70fe0cf77506c632303ec42eec0395
saybaba <kiwanukamarvin597@gmail.com>
2024-06-02 13:37:02 +0300

    testing action

---
+++ hello-world-action/index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
